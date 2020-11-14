0a6ad10ea4b997b79db5c2529f7192a5e01ba99b
PepeTheDog <61550581+PepeTheDog@users.noreply.github.com>
2020-11-14 11:00:13 +0200

    Update joc.ino

--- joc.ino
+++ joc.ino
@@ -1,22 +1,26 @@
 #include <LiquidCrystal_I2C.h>
 LiquidCrystal_I2C lcd(0x3f, 16, 2);
 
-class perete{
-  private:
-    uint8_t coordX;
-    uint8_t coordY;
-  public:
-    perete(){
-      coordX = 
+uint8_t tastatura(uint8_t buttonValue){
+  if (buttonValue<20)
+    return 1;
+  if (buttonValue>=20 && buttonValue<35)
+    return 2;
+  if (buttonValue>=35 && buttonValue<50)
+    return 3;
 }
 
 void setup(){
   lcd.init();
   lcd.backlight();
   lcd.priht("Arduino Game");
+  delay(500);
   lcd.clear();
+  randomSeed(analogRead(A0));
+  Serial.begin(9600);
 }
 
 void loop(){
-  
+  if (tastatura(analogRead(A5)) == 1)
+    Serial.println("buton1");
 }
