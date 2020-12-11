d493f5c5b6c575f31613dbe77379753134047cfc
PepeTheDog <61550581+PepeTheDog@users.noreply.github.com>
2020-12-11 20:08:20 +0200

    Update joc.ino

--- joc.ino
+++ joc.ino
@@ -3,12 +3,15 @@ LiquidCrystal_I2C lcd(0x3f, 16, 2);
 
 byte playerChar[] = {B01100, B01100, B00000, B01110, B11100, B01100, B11010, B10011};
 byte obs[] = {B11111, B11111, B11111, B11111, B11111, B11111, B11111, B11111};
+byte bullet[] = {B00000, B00000, B11100, B11111, B11111, B11100, B00000, B00000};
 
 bool pause = false;
 uint8_t playerHeight = 0;
 uint8_t obsHeight1 = 0, obsHeight2 = 1;
-uint8_t obsPos1, obsPos2;
-uint8_t obsState = 1;
+uint8_t obsPos1 = 16, obsPos2 = 16;
+bool obsMotion1 = true, obsMotion2 = false, bulletMotion = false;
+uint8_t bulletPos = -1, bulletHeight = 1;
+uint8_t frameCounter = 1;
 uint8_t nextObsDistance = 0;
 unsigned long int timp = 0;
 
@@ -21,31 +24,30 @@ uint8_t tastatura(uint16_t buttonValue){
     return 3;
 }
 
-void createObstacle(uint8_t t, uint8_t distance, uint8_t oHeight, uint8_t oPos){
-  if (distance == 0){
-    distance = random(12,16);
-    if (oHeight == 0)
-      oHeight = 1;
-    else oHeight = 0;
-  }
-  if (t%(distance*500) == 0){
-    oPos = 16;
-    lcd.setCursor(oPos,oHeight);
-    lcd.print((byte)1);
-  }
-}
-
 void moveChar(uint8_t pHeight){
   if (tastatura(analogRead(A1)) == 1)
     pHeight = 0;
   else if (tastatura(analogRead(A1)) == 2)
     pHeight = 1;
   lcd.setCursor(0,pHeight);
-  lcd.print((byte)0);
+  lcd.write((byte)0);
+}
+
+void createBullet(bool bMotion, uint8_t bPos, uint8_t bHeight, uint8_t pHeight){
+  if (tastatura(analogRead(A1)) == 3){
+    bMotion = true;
+    bPos = 1;
+    bHeight = pHeight;
+  }
+}
+
+void destroyObs(uint8_t oPos, bool oMotion){
+  oPos = 16;
+  oMotion = false;
 }
 
 void collision(uint8_t oPos, uint8_t oHeight, uint8_t pHeight){
-  if (oPos<=0 && oHeight == pHeight)
+  if (oPos<=0 && oPos>=-3 && oHeight == pHeight)
     pause = true;
 }
 
@@ -57,6 +59,7 @@ void setup(){
   lcd.clear();
   lcd.createChar(0, playerChar);
   lcd.createChar(1, obs);
+  lcd.createChar(2, bullet);
   randomSeed(analogRead(A0));
   Serial.begin(9600);
 }
@@ -64,36 +67,46 @@ void setup(){
 void loop(){
   if (pause == false){
     if (millis() - timp >= 100){
+      Serial.println("frame");
       lcd.clear();
       timp = millis();
       moveChar(playerHeight);
-      if (obsState == 1){
-        createObstacle(timp, nextObsDistance, obsHeight1, obsPos1);
-        obsState = 2;
-      }
-      else if (obsState == 2){
-        createObstacle(timp, nextObsDistance, obsHeight2, obsPos2);
-        obsState = 1;
-      }
-      if (timp%500 == 0){
-        for (uint8_t obsPos1; obsPos1>=-3; obsPos1--){
-          for (uint8_t i=0; i<=3; i++){
-            lcd.setCursor(obsPos1-i,obsHeight1);
-            lcd.print((byte)1);
-          }
+      createBullet(bulletMotion, bulletPos, bulletHeight, playerHeight);
+      if (obsMotion1 == true){
+        for (uint8_t i=0; i<=3; i++){
+          lcd.setCursor(obsPos1+i,obsHeight1);
+          lcd.write((byte)1);
         }
-        for (uint8_t obsPos2; obsPos2>=-3; obsPos2--){
-          for (uint8_t i=0; i<=3; i++){
-            lcd.setCursor(obsPos2-i,obsHeight2);
-            lcd.print((byte)1);
-          }
+      }
+      if (obsMotion2 == true){
+        for (uint8_t i=0; i<=3; i++){
+          lcd.setCursor(obsPos2+i,obsHeight2);
+          lcd.write((byte)1);
         }
-        obsPos1 -= 1;
-        obsPos2 -= 1;
-        nextObsDistance -= 1;
+      }
+      if (bulletMotion == true)
+        lcd.setCursor(bulletPos, bulletHeight);
+        lcd.write((byte)2);
+      if (frameCounter%5 == 0){
+        if (obsMotion1 == true)
+          obsPos1 = obsPos1 - 1;
+        if (obsMotion2 == true){
+          obsPos2 = obsPos2 - 1;
+        if (bulletHeight == 1 || bulletPos >= obsPos1)
+          destroyObs(obsPos1, obsMotion1);
+          bulletMotion = false;
+          bulletPos = -1;
+        if (bulletHeight == 2 || bulletPos >= obsPos2)
+          destroyObs(obsPos2, obsMotion2);
+          bulletMotion = false;
+          bulletPos = -1;
+        nextObsDistance = nextObsDistance - 1;
+        Serial.println("obstacole miscate");
       }//miscare obstacol
       collision(obsPos1, obsHeight1, playerHeight);
       collision(obsPos2, obsHeight2, playerHeight);
+      frameCounter = frameCounter + 1;
+      }
     }
   }//pause false
   else{
